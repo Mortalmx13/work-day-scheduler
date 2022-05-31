a3106b593b79d5bdd0ee41ee221eea609d4b1996
Charles Young <ctyoungiv@gmail.com>
2022-05-31 19:07:18 -0400

    added all the files

---
+++ script.js
@@ -0,0 +1,12 @@
+var rightNowHour = moment().format("H");
+var today = moment().format("MMM Do, YYYY");
+var box9 = document.querySelector("#box9");
+
+
+function displayTimeColors(){
+if(rightNowHour = "9"){
+    
+}
+   
+}
+   
