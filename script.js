fee3adcd9be5b7025048b0e26e8cb089d2f47862
Charles Young <ctyoungiv@gmail.com>
2022-05-31 21:41:27 -0400

    made it so the scheduler changes colors

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 var rightNowHour = moment().format("H");
 var today = moment().format("MMM Do, YYYY");
 var box9 = document.querySelector("#box9");
+// current date at top of page
+$("#currentDay").text(today);
 
 
-function displayTimeColors(){
-if(rightNowHour = "9"){
-    
-}
+$(".time-run").each(function (){
+    var colorNum = parseInt($(this).attr("id"));
+
+    if(colorNum < rightNowHour){
+        $(this).addClass("past");
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+    }else if(colorNum === rightNowHour){
+       
+        $(this).removeClass("past");
+        $(this).addClass("present");
+        $(this).removeClass("future");
+    }else{
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+    } 
    
-}
+})
    
+console.log(rightNowHour);
+console.log(colorNum);
